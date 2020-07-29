a5f5cf4aff634484136ff9ba93954c78a11c4dbc
Goncalo <goncalo.magno@gmail.com>
2020-07-29 19:24:33 -0300

    Build tar package

--- appimage/build-appimage.sh
+++ appimage/build-appimage.sh
@@ -9,9 +9,10 @@ glad --generator c --out-path ext/glad
 rm -rf build
 cmake --config Release -S . -B build -DCMAKE_INSTALL_PREFIX=/usr
 cmake --build build -j $(nproc) --target install DESTDIR=AppDir
+cd build
+cpack -C "Release" -G "STGZ;TGZ"
 
 # build the appimage
-cd build
 wget -nc "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
 chmod +x linuxdeploy-x86_64.AppImage
 cat > igl.desktop <<EOF
